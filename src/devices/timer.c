06ad80eaadb0f505fa2377ec2cb40e095e6942df
Dargones <alexanderfedchin@Alexanders-MacBook-Air.local>
2018-03-01 18:44:38 -0500

    A very rude way to implement timer_sleep. Only works if there is one thread calling timer_sleep at a time!

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -24,6 +24,9 @@ static int64_t ticks;
    Initialized by timer_calibrate(). */
 static unsigned loops_per_tick;
 
+static int awake_time;
+static struct semaphore sem;
+
 static intr_handler_func timer_interrupt;
 static bool too_many_loops (unsigned loops);
 static void busy_wait (int64_t loops);
@@ -34,7 +37,9 @@ static void real_time_delay (int64_t num, int32_t denom);
    and registers the corresponding interrupt. */
 void
 timer_init (void) 
-{
+{ 
+  awake_time = -1;
+  sem.sema_init(0);
   pit_configure_channel (0, 2, TIMER_FREQ);
   intr_register_ext (0x20, timer_interrupt, "8254 Timer");
 }
@@ -90,11 +95,14 @@ void
 timer_sleep (int64_t ticks) 
 {
   int64_t start = timer_ticks (); //what time it is right now
-
   ASSERT (intr_get_level () == INTR_ON); //if the inteupts are enabled
-  while (timer_elapsed (start) < ticks) //if ticks ticks passed since 
+
+  if (awake_time == -1) 
+    awake_time = start + ticks;
+  sem.sema_down();
+  //while (timer_elapsed (start) < ticks) //if ticks ticks passed since 
   // the function was executed first
-    thread_yield ();
+    //thread_yield ();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -172,6 +180,8 @@ static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
+  if ((awake_time != -1) && (ticks >= awake_time)) 
+    sem.sema_up();
   thread_tick ();
 }
 
