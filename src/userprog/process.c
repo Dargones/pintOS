20b18d4ee461166bcccdd5a97c67a735c9b8ae5c
Aleksandr Fedchin <sasha.fedchin@gmail.com>
2018-04-13 15:35:25 -0400

    no_errors

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -440,7 +440,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 /* Create a minimal stack by mapping a zeroed page at the top of
    user virtual memory. */
 static bool
-setup_stack (void **esp, сonst char *argv) 
+setup_stack (void **esp, const char *argv) 
 {
   uint8_t *kpage;
   bool success = false;
